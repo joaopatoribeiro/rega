679da04e74f3df7b2f46ffbec7c6d0521ad2376f
joaopatoribeiro <66251204+joaopatoribeiro@users.noreply.github.com>
2020-08-09 22:52:49 +0100

    first file attempt

--- backend_GPIO.py
+++ backend_GPIO.py
@@ -21,3 +21,24 @@ class Relay(object):
         except:
             return False
         return True
+class DHT_SENSOR (object):
+    def __init__(self, pin):
+        self.pin = pin
+        self.temperature,self.humidity = DHT.read(DHT.DHT11,self.pin)
+    def read(self):
+        try :
+            self.temperature, self.humidity = DHT.read(DHT.DHT11, self.pin)
+        except:
+            return False
+        return (self.temperature, self.humidity)
+
+class READ_CPU_TEMP (object):
+    def __init__(self):
+        self.cpu_temp = self.read_cpu_temp()
+
+    def read_cpu_temp(self):
+        tfile=open('/sys/class/thermal/thermal_zone0/temp')
+        temp = float(tfile.read())
+        tempC= temp/1000
+        tfile.close()
+        return tempC

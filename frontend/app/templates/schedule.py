9bdcea74d3bbc82c1db6c69b192616dbfcb4cd5e
joaopatoribeiro <pato_ribeiro@hotmail.com>
2020-08-19 12:06:13 +0100

    forms

---
+++ frontend/app/templates/schedule.py
@@ -0,0 +1,21 @@
+{% extends "base.html" %}
+
+
+{% block content %}
+
+<h1> {{ form.name }} <h1>
+<form action="" method="post" novalidate>
+    {{ form.hidden_tag() }}
+    <p> 
+        {{ form.time.lable }}
+        {{ form.time(size=20) }} 
+    </p>
+    <p>
+        {{ form.circuit.lable }}
+        {{ form.circuiti(size=20) }}}
+    </p>
+
+    <p> {{ form.submit() }} </p>
+</form>
+
+{{%endblock%}}
